a6d425f6fe502d45ab4c8ffb1dee70262f361e09
= <dodechotomy@gmail.com>
2020-05-01 14:50:04 -0600

    Added grid point rendering

--- sketch.js
+++ sketch.js
@@ -1,15 +1,31 @@
 let grid;
 let flowfield;
+let gridDetail;
+let gap;
+let margin;
+let gridSize;
+let gridCellSize;
+let cellSize;
 
 function setup() {
-  createCanvas(1080, 1080);
-  const gridDetail = 10;
-  const gap = width / gridDetail;
+  createCanvas(800, 800);
+  rectMode(CENTER);
+  gridDetail = 50;
+  margin = 2;
+  gridSize = width - margin;
+  gridCellSize = gridSize / gridDetail;
+  cellSize = gridCellSize - margin;
+
+  // cellSize = gap-margin;
   grid = createGrid({
     dimensions: {
+      origin: {
+        x: margin/2 + gridCellSize/2,
+        y: margin/2 + gridCellSize/2
+      },
       x: gridDetail,
       y: gridDetail,
-      gap: gap
+      gap: gridCellSize
     }
   });
   flowfield = new Flowfield();
@@ -17,8 +33,18 @@ function setup() {
 
 function draw() {
   background('#19c');
+  noStroke();
   grid.points.forEach((point) => {
-    ellipse(point.x, point.y, 20);
+    push();
+    const value = flowfield.sample(point);
+    translate(point.x, point.y)
+    rotate(value);
+    fill('#07a')
+    ellipse(0,0,cellSize, cellSize);
+    stroke("#000");
+    strokeWeight(2);
+    line(0,0,cellSize/2,0);
+    pop();
   });
 
 }
