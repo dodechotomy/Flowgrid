3057f055ab424ce5186c906980d163cebe056094
= <dodechotomy@gmail.com>
2020-05-22 17:51:27 -0600

    Added flowfield walker and auto image saving

--- sketch.js
+++ sketch.js
@@ -8,6 +8,11 @@ let gridCellSize;
 let cellSize;
 let colorA, colorB;
 let palette;
+let walkers = [];
+let framesToRender = 0;
+let startFrame = 0;
+let savedCount=0;
+let normalLifespan = 100;
 
 function setup() {
   createCanvas(800, 800);
@@ -15,21 +20,25 @@ function setup() {
   reset();
 }
 
-function keyPressed(){
-  if( key  === 'n'){
+function keyPressed() {
+  if (key === 'n') {
     reset();
   }
 }
-function reset(){
-  gridDetail = 50;
-  margin = -1;
+
+function reset() {
+  gridDetail = 200;
+  margin = 10;
   gridSize = width - margin;
   gridCellSize = gridSize / gridDetail;
   cellSize = gridCellSize - margin;
-  palette = Palette.twoColorPalette();
+  // palette = Palette.limitedPalette(3);
+  palette = new Palette();
   // colorMode(HSB);
   // const hue = Math.random()*100;
   perlin.seed(Math.random());
+  background(  palette.nextColor());
+
   colorA = palette.nextColor();
   colorB = palette.nextColor();
   // cellSize = gap-margin;
@@ -42,33 +51,66 @@ function reset(){
   });
   flowfield = new Flowfield({
     dimensions: {
-      range: [-20, width + 20],
+      range: [-50, width + 50],
       detail: gridDetail
     }
   });
   flowfield.initialize();
+
+  normalLifespan = Math.random()*1000;
+  walkers = [];
+  const walkerCount = Math.exp(Math.random()*11+2);
+  for (let i = 0; i < walkerCount; i++) {
+    walkers.push(new Walker(flowfield, palette,normalLifespan));
+  }
+
+
+  framesToRender = Math.random()*1200;
+  startFrame = frameCount;
 }
 
 function draw() {
-  // background('#07a');
-  flowfield.initialize();
+  // background(0);
+  // background(palette.background());
+  flowfield.mutate();
   noStroke();
+  /*
   flowfield.grid.points.flat().forEach((point) => {
     push();
-    const value = point.value; //flowfield.sample(point);
+    // const value = Math.abs(point.value*2 - 1)/2; //flowfield.sample(point);
+    const value = point.value % 1; //flowfield.sample(point);
     translate(point.x, point.y)
-    // rotate(value*TWO_PI);
-    // const colorA = color('#07a');
-    // const colorB = color('#000');
-    const colorC = lerpColor(colorA,colorB,value);
+
+    let angle = value;
+    // const rounding = 256;
+    // angle = Math.round(value*rounding)/rounding;
+    angle = map(value, 0, 1, 0, TWO_PI);
+    let factor = Math.abs(value - 0.5) * 2;
+    colorMode(RGB);
+    const colorC = lerpColor(colorA, colorB, factor);
+    colorMode(HSB);
     fill(colorC)
     rect(0, 0, cellSize, cellSize);
     // fill('#000')
     // ellipse(-10, 0, cellSize, cellSize);
-    // stroke("#000");
-    // strokeWeight(2);
     // line(-10, 0, cellSize / 2, 0);
+    stroke(colorC);
+    strokeWeight(2);
+    rotate(angle);
+    line(0, 0, cellSize, cellSize)
     pop();
   });
+*/
+  walkers.forEach(w=>w.move());
+  walkers.forEach(w=>w.draw());
+
+  if(frameCount-startFrame > framesToRender){
+    save(`image${frameCount}frames${Math.floor(framesToRender)}.png`);
+    savedCount++;
+    if(savedCount>200){
+      noLoop();
+    }
+    reset();
+  }
 
 }
