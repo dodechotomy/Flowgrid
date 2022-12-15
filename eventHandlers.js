4d1774f02c943106f1e36cfdfab58258d2693695
Scott Wilson <me.scottwilson@gmail.com>
2022-12-15 13:48:36 +1300

    Added trail fading, reworked many things

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,7 +1,7 @@
 const controls = {
     autoAdvance: {
         tag: "auto-advance",
-        attachEventHandlers: (element) => {
+        attach: (element) => {
             element.onchange = (e) => {
                 autoAdvanceMode = e.target.checked;
                 console.log(e.target.name + " toggled to " + e.target.checked)
@@ -12,7 +12,18 @@ const controls = {
     },
     autoSave: {
         tag: "auto-save",
-        attachEventHandlers: (element) => {
+        attach: (element) => {
+            element.onchange = (e) => {
+                savingFinals = e.target.checked;
+                console.log(e.target.name + " toggled to " + e.target.checked)
+            };
+            element.checked = savingFinals;
+
+        }
+    },
+    saveFrames: {
+        tag: "save-frames",
+        attach: (element) => {
             element.onchange = (e) => {
                 savingFrames = e.target.checked;
                 console.log(e.target.name + " toggled to " + e.target.checked)
@@ -21,19 +32,20 @@ const controls = {
 
         }
     },
-    overlay: {
-        tag: "overlay",
-        attachEventHandlers: (element) => {
+    animate: {
+        tag: "animate",
+        attach: (element) => {
             element.onchange = (e) => {
-                overlayMode = e.target.checked;
+                animateMode = e.target.checked;
                 console.log(e.target.name + " toggled to " + e.target.checked)
             };
-            element.checked = overlayMode;
+            element.checked = animateMode;
+
         }
     },
     next: {
         tag: "next",
-        attachEventHandlers: (element) => {
+        attach: (element) => {
             element.onclick = (e) => {
                 generate();
                 console.log(e.target.name + " button clicked");
@@ -42,13 +54,63 @@ const controls = {
     },
     save: {
         tag: "save",
-        attachEventHandlers: (element) => {
+        attach: (element) => {
             element.onclick = (e) => {
                 saveFrame();
                 console.log(e.target.name + " button clicked");
             };
         }
     },
+    density: {
+        tag: "walker-density",
+        attach: (element) => {
+            element.onchange = (e) => {
+                walkerDensity = parseFloat(e.target.value);
+                console.log(e.target.name + ": " + e.target.value);
+            };
+            element.value = walkerDensity;
+        }
+    },
+    lifespanMin: {
+        tag: "lifespan-min",
+        attach: (element) => {
+            element.onchange = (e) => {
+                lifeSpan[0] = parseFloat(e.target.value);
+                console.log(e.target.name + ": " + e.target.value);
+            };
+            element.value = lifeSpan[0];
+        }
+    },
+    lifespanMax: {
+        tag: "lifespan-max",
+        attach: (element) => {
+            element.onchange = (e) => {
+                lifeSpan[1] = parseFloat(e.target.value);
+                console.log(e.target.name + ": " + e.target.value);
+            };
+            element.value = lifeSpan[1];
+        }
+    },
+    resolutionX: {
+        tag: "resolution-x",
+        attach: (element) => {
+            element.onchange = (e) => {
+                resolution[0] = parseInt(e.target.value);
+                console.log(e.target.name + ": " + e.target.value);
+            };
+            element.value = resolution[0];
+        }
+    },
+    resolutionY: {
+        tag: "resolution-y",
+        attach: (element) => {
+            element.onchange = (e) => {
+                resolution[1] = parseInt(e.target.value);
+                console.log(e.target.name + ": " + e.target.value);
+            };
+            element.value = resolution[1];
+        }
+    }
 
 };
 function ready() {
@@ -56,7 +118,7 @@ function ready() {
     console.log("attaching click handlers");
     Object.values(controls).forEach(control => {
         let element = document.getElementById(control.tag);
-        control.attachEventHandlers(element);
+        control.attach(element);
     });
 };
 document.addEventListener("DOMContentLoaded", ready);
--- sketch.js
+++ sketch.js
@@ -1,54 +1,58 @@
 let grid;
 
 let savingFrames = false;
-let overlayMode = false;
+let savingFinals = false;
 let autoAdvanceMode = false;
+let animateMode = false;
 
-let flowfieldA;
-let flowfieldB;
-let flowfieldMask;
 
 let gridDetail;
 let gap;
 let margin;
 let gridSize;
 
-let paletteA;
-let paletteB;
-let paletteMask;
-
-let walkersA;
-let walkersB;
-let walkersMask;
+let graphics;
+let flowfield;
+let palette;
+let secondPalette;
+let walkers;
+let group;
 
 let framesToRender = 0;
 let startFrame = 0;
 let savedCount = 0;
-let maxSavedFrames = 200;
+let maxSavedFrames = 50;
 
-let graphicsA;
-let graphicsB;
-let graphicsMask;
 
-let walkerDensity = 3;
+let walkerDensity = 0.5;
 const walkerDensityDenominator = 2000;
+const sizeRange = [-0.5,1.5];
+const sizeScale = s=>map(s,0,1,sizeRange[0],sizeRange[1]);
 
-let lifeSpan = [50, 250]
+let lifeSpan = [100, 300];
 
 let livingWalkers = () => walkersA.active().length + walkersB.active().length + walkersMask.active().length;
 
-let groupA;
-let groupB;
-let groupMask;
 
-const resolution = [400, 400];
+let resolution = [1920, 1080];
+let params = new URLSearchParams(document.location.search);
+if(params.has("width")){
+  let w = parseInt(params.get("width"));
+  if(w > 0){
+    resolution[0] = w;
+  }
+}
+if(params.has("height")){
+  let h = parseInt(params.get("height"));
+  if(h > 0){
+    resolution[1] = h;
+  }
+}
 // let imageComp;
 
 function setup() {
   createCanvas(resolution[0], resolution[1]);
-  graphicsA = createGraphics(resolution[0], resolution[1]);
-  graphicsB = createGraphics(resolution[0], resolution[1]);
-  graphicsMask = createGraphics(resolution[0], resolution[1]);
+  graphics = createGraphics(resolution[0], resolution[1]);
   reset();
   generate();
 }
@@ -58,123 +62,138 @@ function saveFrame() {
   savedCount++;
   if (savedCount > maxSavedFrames) {
     noLoop();
+    autoAdvanceMode = false;
   }
 }
 
 function reset() {
-  gridDetail = 200;
-  // palette = Palette.limitedPalette(3);
-  paletteA = Palette.normalPalette()
-  paletteB = Palette.normalPalette()
-  // paletteMask = Palette.normalPalette();
-  paletteMask = Palette.bw();
+  if( !(resolution[0] === width && resolution[1] === height)){
+    resizeCanvas(resolution[0], resolution[1], true); //resize canvas without redrawing
+    graphics = createGraphics(resolution[0], resolution[1]);
+  }
+  gridDetail = 5;
+  palette = Palette.createPalette()
+  if(palette.colors.length > 4){
+    secondPalette = palette.split();
+  }else{
+    secondPalette = palette;
+  }
 
   perlin.seed(Math.random());
-  // if (Math.random() < 0.5) {
-  graphicsA.background(paletteA.background());
-  graphicsB.background(paletteB.background());
-  graphicsMask.background(paletteMask.background());
+  // graphics.background(palette.background());
 
 
-  graphicsA.noStroke();
-  graphicsB.noStroke();
-  graphicsMask.noStroke();
+  graphics.noStroke();
 
   margin = -0.2;
   const options = {
-    type: "square",
+    type: "rectangle",
     dimensions: {
       detail: gridDetail,
       range: [width * margin, width - width * margin, height * margin, height - height * margin]
     }
   }
 
-  flowfieldA = new Flowfield(createGrid(options));
-  flowfieldA.initialize();
-
-  flowfieldB = flowfieldA.clone();
-  flowfieldB.mutate();
-
-  flowfieldMask = flowfieldA.clone();
-  flowfieldMask.mutate();
+  flowfield = new Flowfield(createGrid(options));
+  flowfield.initialize();
 
 
   const walkerCount = () => walkerDensity/walkerDensityDenominator * resolution[0] * resolution[1];
   const normalLifespan = () => lifeSpan[0] + (lifeSpan[1] - lifeSpan[0]) * Math.random();
 
-  walkersA = new WalkerGroup(flowfieldA, paletteA, normalLifespan());
-  walkersB = new WalkerGroup(flowfieldB, paletteB, normalLifespan());
-  walkersMask = new WalkerGroup(flowfieldMask, paletteMask, normalLifespan());
+  walkers = new WalkerGroup(flowfield, palette, normalLifespan());
 
-  walkersA.spawn(walkerCount());
-  walkersB.spawn(walkerCount());
-  walkersMask.spawn(walkerCount());
-
-
-
-  // framesToRender = Math.random() * 600;
-  // startFrame = frameCount;
+  walkers.spawn(walkerCount());
 }
 
 function draw() {
   clear();
-  if (autoAdvanceMode) {
+  graphics.clear();
+  if (autoAdvanceMode && walkers.complete()) {
+    if (savingFinals) {
+      saveFrame();
+    }
     generate();
   }
-  if (overlayMode) {
-    fullDraw();
-  } else {
-    drawGraphics(graphicsA);
+  if(animateMode){
+    flowfield.mutate();
+    walkers.move();
+    walkers.draw(graphics,false);
+  }else{
+    //starting ff
+    walkers.fastForward();
+    //done ff
+    //starting walker draw
+    walkers.draw(graphics,true);
+    //done walker draw
+    if(!autoAdvanceMode){
+      noLoop();
+    }
   }
-  if (savingFrames === true) {
+  drawFlowfield(flowfield);
+  drawGraphics(graphics);
+  if (savingFrames) {
     saveFrame();
   }
-
+  // clear();
 }
 
 function generate() {
   reset();
-  flowfieldA.mutate();
-  flowfieldB.mutate();
-  flowfieldMask.mutate();
-
-  walkersA.fastForward();
-  walkersB.fastForward();
-  walkersMask.fastForward();
-
-  walkersA.draw(graphicsA);
-  walkersB.draw(graphicsB);
-  walkersMask.draw(graphicsMask);
+  flowfield.mutate();
+  loop();
 }
 
-function fullDraw() {
-  graphicsA.loadPixels();
-  graphicsB.loadPixels();
-  graphicsMask.loadPixels();
-  loadPixels();
-  const d = pixelDensity();
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      for (let i = 0; i < d; i++) {
-        for (let j = 0; j < d; j++) {
-          // loop over
-          index = 4 * ((y * d + j) * width * d + (x * d + i));
-
-          const a = graphicsMask.pixels[index] / 255;
-          const b = 1 - a;
-
-          pixels[index] = graphicsA.pixels[index] * a + graphicsB.pixels[index] * b;
-          pixels[index + 1] = graphicsA.pixels[index + 1] * a + graphicsB.pixels[index + 1] * b;
-          pixels[index + 2] = graphicsA.pixels[index + 2] * a + graphicsB.pixels[index + 2] * b;
-          pixels[index + 3] = graphicsA.pixels[index + 3] * a + graphicsB.pixels[index + 3] * b;
-        }
-      }
-    }
-  }
-  updatePixels();
-}
 
 function drawGraphics(graphics) {
-  clear();
   image(graphics, 0, 0);
+}
+
+function filterColor(options){
+
+  options.graphics.colorMode(HSB);
+  let h = hue(options.color);
+  let s = saturation(options.color);
+  let b = brightness(options.color);
+  let a = alpha(options.color);
+
+  let sizeFactor = map(options.size,1,100,0,1,true);
+  sizeFactor = sizeFactor * sizeFactor;
+  a  = map(sizeFactor,0,1,0,1, true); //make large circles transparent
+  // a = a % 1;
+
+
+  let ageFactor = map(options.age, 0, options.lifespan,0,1,true);
+  ageFactor = Math.abs(0.5 - ageFactor)*2;
+  a  = 1-ageFactor;
+  a = a * a * a;
+
+  if(a <= 0){
+    return null;
+  }
+
+  return color(h,s,b,a);
+  
+}
+
+function drawFlowfield(flowfield){
+    colorMode(HSB, 1);
+    noFill();
+    noStroke();
+    let bg = palette.background();
+    let bg1 = palette.nextColor(bg);
+    flowfield.grid.points.flat().forEach(point=>{
+      let x0 = point.x;
+      let y0 = point.y;
+      // let v = p5.Vector.fromAngle(point.value.direction*TWO_PI);
+      // v.setMag(point.value.strength*20);
+    //  let c = palette.colorBlend(point.value.size);
+      let c = bg;
+      if(bg1){
+      c = lerpColor(bg, bg1, point.value.lifespan);
+      }
+      fill(c);
+      circle(x0,y0,20);
+    });
+    // filter(BLUR,1);
 }
